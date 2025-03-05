8d5bc440ea9d882c0567005ad07c8939178655a9
ashamweene <82180635+a-mwen@users.noreply.github.com>
2025-03-05 11:34:13 -0500

    fixed server issues

--- task_manager_backend/database.js
+++ task_manager_backend/database.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Create a new pool with your database credentials
 const pool = new Pool({
-    user: process.env.DB_USER ,
-    host: process.env.DB_HOST ,
-    database: process.env.DB_NAME ,
-    password: process.env.DB_PASSWORD ,  // Add your DB password here
-    port: process.env.DB_PORT, // Default PostgreSQL port
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for connecting to Render's hosted PostgreSQL
+    }
 });
 
-// Export the pool to use it in other files
 module.exports = pool;
